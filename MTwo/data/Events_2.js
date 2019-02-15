d10b03b0d5f71061ae295cc3ecc6f52bab53c2b4
Clayton Anzalone <42486174+thecaliconoire@users.noreply.github.com>
2019-02-15 12:19:50 +0100

    Add files via upload

---
+++ MTwo/data/Events_2.js
@@ -0,0 +1,8 @@
+var json_Events_2 = {
+"type": "FeatureCollection",
+"name": "Events_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+
+]
+}
---
+++ MTwo/data/PastEvents_1.js
@@ -0,0 +1,8 @@
+var json_PastEvents_1 = {
+"type": "FeatureCollection",
+"name": "PastEvents_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+
+]
+}
